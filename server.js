21ed2803f5ce050b2dec32fd27cd448ecea883cd
Unknown <petinari@outlook.com>
2018-04-24 22:47:00 -0400

    1

---
+++ server.js
@@ -0,0 +1,23 @@
+const _express = require('express')
+
+var app = _express()
+
+app.get('/', (req, resp) => {
+    resp.send({
+        nome : 'Robson',
+        likes : [
+            'Moto',
+            'Comida',
+            'Dinheiro'
+        ]
+    })
+})
+
+app.get('/about', (req, resp) => {
+    resp.send('About Page')
+})
+
+
+
+
+app.listen(1986)
