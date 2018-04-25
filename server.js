b73e55690eed18e0b2fbae77bc88d5580e95ed2b
Unknown <petinari@outlook.com>
2018-04-25 10:43:22 -0400

    6

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const _express = require('express')
-// const _hbs = require('hbs');
+const _hbs = require('hbs');
+
 
 var app = _express()
 
+_hbs.registerPartials(__dirname + '/views/partials')
+_hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear()
+})
 
 app.set('view engine', 'hbs')
 app.use(_express.static(__dirname + '/public'))
@@ -11,14 +16,14 @@ app.get('/', (req, resp) => {
     resp.render('home.hbs', {
         welcomeMessage: 'Welcome to some website',
         pageTitle: 'Home Page',
-        currentYear: new Date().getFullYear()
+       
     })
 })
 
 app.get('/about', (req, resp) => {
     resp.render('about.hbs', {
         pageTitle: 'About Page',
-        currentYear: new Date().getFullYear()
+      
     })
 })
 
@@ -26,5 +31,5 @@ app.get('/about', (req, resp) => {
 
 
 app.listen(2010, () => {
-    console.log('Server is running on port 2010' )
+    console.log('Server is running on port 2010')
 })
